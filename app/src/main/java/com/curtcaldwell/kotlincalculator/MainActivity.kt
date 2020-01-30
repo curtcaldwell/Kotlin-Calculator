bf15095a859b71c50110abf26b67b0abe3cb5318
Curt Caldwell <curt.caldwell24@gmail.com>
2020-01-29 16:08:47 -0800

    updated rotation

--- app/src/main/java/com/curtcaldwell/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/curtcaldwell/kotlincalculator/MainActivity.kt
@@ -4,9 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import java.lang.NumberFormatException
+import kotlinx.android.synthetic.main.activity_main.*
 
 private const val STATE_PENDING_OPERATION =  "PendingOperation"
 private const val STATE_OPERAND1 =  "OPERAND1"
@@ -15,10 +14,6 @@ private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var result: EditText
-    private lateinit var newNumber: EditText
-    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
 
     //Variables to hold the operands and type of calculation
 
@@ -31,32 +26,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        result = findViewById(R.id.result)
-        newNumber = findViewById(R.id.new_number)
-
-        // Data Input Buttons
-        val button0: Button = findViewById(R.id.button0)
-        val button1: Button = findViewById(R.id.button1)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
-        val button4: Button = findViewById(R.id.button4)
-        val button5: Button = findViewById(R.id.button5)
-        val button6: Button = findViewById(R.id.button6)
-        val button7: Button = findViewById(R.id.button7)
-        val button8: Button = findViewById(R.id.button8)
-        val button9: Button = findViewById(R.id.button9)
-        val buttonDot: Button = findViewById(R.id.buttonDot)
-
-        //Operation Buttons
-        val buttonEquals: Button = findViewById(R.id.buttonEquals)
-        val buttonDivide: Button = findViewById(R.id.buttonDivide)
-        val buttonMultiply: Button = findViewById(R.id.buttonMultiply)
-        val buttonMinus: Button = findViewById(R.id.buttonMinus)
-        val buttonPlus: Button = findViewById(R.id.buttonAddition)
-
         val clickListener = View.OnClickListener { v ->
             val b = v as Button
-            newNumber.append(b.text)
+            new_number.append(b.text)
         }
 
         buttonDot.setOnClickListener(clickListener)
@@ -71,25 +43,47 @@ class MainActivity : AppCompatActivity() {
         button9.setOnClickListener(clickListener)
         button0.setOnClickListener(clickListener)
 
+
+
         val opListener = View.OnClickListener { v ->
             val op = (v as Button).text.toString()
             try {
-                val value = newNumber.text.toString().toDouble()
+                val value = new_number.text.toString().toDouble()
                 performOperation(value, op)
 
             } catch (e: NumberFormatException) {
-                newNumber.setText("")
+                new_number.setText("")
             }
 
             pendingOperation = op
-            displayOperation.text = pendingOperation
+            operation.text = pendingOperation
         }
+
+
 
         buttonEquals.setOnClickListener(opListener)
         buttonMinus.setOnClickListener(opListener)
-        buttonPlus.setOnClickListener(opListener)
+        buttonAddition.setOnClickListener(opListener)
         buttonDivide.setOnClickListener(opListener)
         buttonMultiply.setOnClickListener(opListener)
+
+        buttonNeg.setOnClickListener{ v ->
+            val value = new_number.text.toString()
+            if(value.isEmpty() ){
+                new_number.setText("-")
+            } else{
+                try {
+                    var doubleValue = value.toDouble()
+                    doubleValue *= -1
+                    new_number.setText(doubleValue.toString())
+
+                } catch(e: NumberFormatException){
+                    new_number.setText("-")
+                }
+            }
+
+
+        }
 
     }
 
@@ -117,7 +111,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         result.setText(operand1.toString())
-        newNumber.setText("")
+        new_number.setText("")
 
 
     }
@@ -142,7 +136,7 @@ class MainActivity : AppCompatActivity() {
         operand1 = savedInstanceState.getDouble(STATE_OPERAND1)
         pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION)
 
-        displayOperation.text = pendingOperation
+        operation.text = pendingOperation
     }
 
 
